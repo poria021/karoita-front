6fcf7320204abfd80e78df82c9df5c229638bf75
carozamani <carozamani@proton.me>
2026-08-19 08:31:27 +0330

    fix(ui): remove FontAwesome CSS import from Providers to prevent FOUI

--- src/components/shared/shell/Providers.tsx
+++ src/components/shared/shell/Providers.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { config } from '@fortawesome/fontawesome-svg-core';
-import '@fortawesome/fontawesome-svg-core/styles.css';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { Direction } from 'radix-ui';
 import { ThemeProvider } from 'next-themes';
